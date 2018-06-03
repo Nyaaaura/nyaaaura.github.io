a7c4bca5150ead350560be58f7a1ccadb769cfb5
Nyaaaura <brandon03710@gmail.com>
2018-06-02 23:43:09 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -150,12 +150,5 @@ $(document).ready(function() {
         duplicated: true,
     })
 
-	window.SetVolume = function(val)
-	{
-    var player = document.getElementById('bg');
-    console.log('Before: ' + player.volume);
-    player.volume = val / 100;
-    console.log('After: ' + player.volume);
-	}
 	
 	});
